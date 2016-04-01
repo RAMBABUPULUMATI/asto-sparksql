76c864555c01d82df1e9937d93207428ecc2d37b
Lucius <zj_lingxin@163.com>
2016-04-01 17:02:43 +0800

    spark sql 项目基本结果

---
+++ src/main/scala/com/asto/dmp/xxx/dao/impl/BaseDao.scala
@@ -0,0 +1,11 @@
+package com.asto.dmp.xxx.dao.impl
+
+import com.asto.dmp.xxx.dao.{SQL, Dao}
+
+object BaseDao extends Dao {
+
+/*  private def getOrderDetailsProps(sql: SQL = new SQL()) = {
+    //getProps(Constants.InputPath.ORDER_DETAILS, Constants.Schema.ORDER_DETAILS, "tobacco_order_details", sql)
+  }*/
+
+}
---
+++ src/main/scala/com/asto/dmp/xxx/service/impl/ScoreService.scala
@@ -0,0 +1,19 @@
+package com.asto.dmp.xxx.service.impl
+
+
+import com.asto.dmp.xxx.service.Service
+import org.apache.spark.Logging
+
+object ScoreService extends Logging {
+
+
+
+}
+
+class ScoreService extends Service {
+  override def runServices() = {
+
+  }
+}
+
+
